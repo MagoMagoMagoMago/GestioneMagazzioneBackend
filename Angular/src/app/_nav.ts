51ffb382f6efc7bbb906a315fb552e00b14f8ced
Fago Matilde <matilde.fago@tecnicosuperiorekennedy.it>
2021-06-07 19:17:36 +0200

    template angular

---
+++ Angular/src/app/_nav.ts
@@ -0,0 +1,34 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle'
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      }
+    ]
+  }
+];
