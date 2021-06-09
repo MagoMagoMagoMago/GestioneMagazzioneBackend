a90d7e0efe685d2c6953d88cddb06248723931a3
DardanBaljaj <dardan.baljaj.11@gmail.com>
2021-06-09 22:20:20 +0200

    template frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "**",
+    component: ErrorPageComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
