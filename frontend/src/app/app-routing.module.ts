00addb9996d8dbe0907b5c15dcdc61ee98ea9a23
DardanBaljaj <dardan.baljaj.11@gmail.com>
2021-06-10 16:19:08 +0200

    Navbar dinamica

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,18 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {
     path: "",
-    component: LoginComponent
+    component: LoginComponent,
+    data: {
+      title: 'Login'
+    }
   },
   { path: 'items', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) },
   {
     path: "**",
-    component: ErrorPageComponent
+    component: ErrorPageComponent,
+    data: {
+      title: 'Not Found'
+    }
   },
 ];
 
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Navbar } from './navbar';
 
 @Component({
   selector: 'app-header',
@@ -7,17 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-
+  public navbar: Navbar[] = [
+    {
+      title: "Dashboard ",
+      icon: "bootstrap-icons.svg#speedometer2",
+      hover: false,
+      link: "home",
+      visible: true
+    },
+    {
+      title: "Ordini",
+      icon: "bootstrap-icons.svg#table",
+      hover: false,
+      link: "home",
+      visible: true
+    },
+    {
+      title: "Articoli",
+      icon: "bootstrap-icons.svg#grid",
+      hover: false,
+      link: "home",
+      visible: true
+    },
+    {
+      title: "Acquisti",
+      icon: "bootstrap-icons.svg#bag",
+      hover:false,
+      link:"home",
+      visible: true
+    },
+    {
+      title: "Fornitori",
+      icon: "bootstrap-icons.svg#people",
+      hover:false,
+      link:"home",
+      visible: true
+    },
+    {
+      title: "Profilo",
+      icon: "bootstrap-icons.svg#person-circle",
+      hover: false,
+      link: "home",
+      visible: true
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onHover(event: any): void {
-    event.target.className = String(event.target.className).replace('text-white', 'text-info');
+  onHover(item: Navbar): void {
+    // event.target.className = String(event.target.className).replace('text-white', 'text-warning');
+    item.hover = true;
   }
 
-  removeHover(event: any): void {
-    event.target.className = String(event.target.className).replace('text-info', 'text-white');
+  removeHover(item: Navbar): void {
+    // event.target.className = String(event.target.className).replace('text-warning', 'text-white');
+    item.hover = false;
   }
 }
