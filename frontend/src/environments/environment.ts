4735e188326798e907f3af75b670557c86529ee3
DardanBaljaj <dardan.baljaj.11@gmail.com>
2021-06-16 12:11:56 +0200

    endpoints

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  host: "localhost:8000/",
+  endpoint: {
+    items: "items",
+    orders: "orders",
+    purchases: "purchases",
+    login: "login"
+  }
 };
 
 /*
