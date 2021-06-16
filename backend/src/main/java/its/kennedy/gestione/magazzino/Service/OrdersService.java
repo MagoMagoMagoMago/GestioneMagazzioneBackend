4e05b0c122da6609b06f24c85c5d307330cdc87b
Manuel Maraga <Manuel.Maraga@corvallis.it>
2021-06-16 10:38:46 +0200

    added orders controller and service

--- backend/src/main/java/its/kennedy/gestione/magazzino/Service/OrdersService.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/Service/OrdersService.java
@@ -6,6 +6,10 @@ import its.kennedy.gestione.magazzino.IService.IOrders;
 import its.kennedy.gestione.magazzino.Repository.OrdersRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,12 +31,34 @@ public class OrdersService implements IOrders {
     public Boolean addOrders(List<OrdersDto> orders) {
         List<Orders> ordersDao = new ArrayList<>();
         orders.forEach(o -> {
+
             ordersDao.add(modelMapper.map(o, Orders.class));
         });
         if (ordersRepository.saveAll(ordersDao).size() == orders.size()) {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public List<OrdersDto> selezionaPagina(int pagina, int quantita, String sortBy, Boolean dir) {
+        Pageable p;
+        if (sortBy.length() <= 0) {
+            sortBy = "Id";
+        }
+        if (dir == true) {
+            p = PageRequest.of(pagina, quantita, Sort.by(sortBy).ascending());
+        } else {
+            p = PageRequest.of(pagina, quantita, Sort.by(sortBy).descending());
+        }
+
+
+        Page<Orders> resP = ordersRepository.findAll(p);
+        ArrayList<OrdersDto> res = new ArrayList<OrdersDto>();
+        for (Orders d : resP) {
+            res.add(modelMapper.map(ordersRepository.getById(d.getAmazonOrderId()), OrdersDto.class));
+        }
+        return res;
     }
 
 
