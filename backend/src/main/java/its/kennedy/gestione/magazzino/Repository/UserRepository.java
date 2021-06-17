45f4df71f2ca7bdbeac89b5db1ecc0f268f0ec1d
DardanBaljaj <dardan.baljaj.11@gmail.com>
2021-06-17 22:55:44 +0200

    JWT Auth (BackEnd) ----> done

--- backend/src/main/java/its/kennedy/gestione/magazzino/Repository/UserRepository.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/Repository/UserRepository.java
@@ -1,17 +1,22 @@
 package its.kennedy.gestione.magazzino.Repository;
 
-import its.kennedy.gestione.magazzino.Dao.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import its.kennedy.gestione.magazzino.Dao.User;
+
 
 /**
- * Spring Data repository for the UserDao entity.
+ * Spring Data repository for the User entity.
  */
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmailAndPassword(String email, String password);
+	Optional<User> findByUsername(String username);
 
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
 }
