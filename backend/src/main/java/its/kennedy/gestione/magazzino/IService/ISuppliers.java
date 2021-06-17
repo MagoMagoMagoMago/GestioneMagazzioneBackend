b46093bc103adb9704ca4ee856fe4bb919e2df90
@michael.brotto <michael.brotto@tecnicosuperiorekennedy.it>
2021-06-17 15:32:34 +0200

    supplier modica e gat all

--- backend/src/main/java/its/kennedy/gestione/magazzino/IService/ISuppliers.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/IService/ISuppliers.java
@@ -1,5 +1,13 @@
 package its.kennedy.gestione.magazzino.IService;
 
+import java.util.List;
+
+import its.kennedy.gestione.magazzino.Dao.Suppliers;
+import its.kennedy.gestione.magazzino.Dto.SuppliersDto;
+
 public interface ISuppliers {
+	SuppliersDto getById(Integer id);
+	Boolean modifica(Suppliers entity);
+	List<SuppliersDto> getAll();
 
 }
