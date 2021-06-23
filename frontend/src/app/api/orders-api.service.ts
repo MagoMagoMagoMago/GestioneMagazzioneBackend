68cbea63fd1203d96b156d1cc675a9f7156c246d
Fago Matilde <matilde.fago@tecnicosuperiorekennedy.it>
2021-06-23 09:58:43 +0200

    getAll orders, pagination

--- frontend/src/app/api/orders-api.service.ts
+++ frontend/src/app/api/orders-api.service.ts
@@ -12,7 +12,8 @@ export class OrdersApiService {
 
   constructor(private http: HttpClient) { }
 
-  public getAll(ordinamento: string, pagine: number, quantita: number ): Observable<any> {
-    return this.http.get(this.host + "/pagina/" + ordinamento + "/" + pagine + "/" + quantita );
-    
-  }}
+  public getAll(sortBy: string, orderBy: boolean,  pagine: number, quantita: number ): Observable<any> {
+    return this.http.get(this.host + "/pagina/" + sortBy + "/" + orderBy + "/" + pagine + "/" + quantita );
+  }
+
+}
--- frontend/src/app/views/orders/orders.component.ts
+++ frontend/src/app/views/orders/orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersApiService } from 'src/app/api/orders-api.service';
+import { Column } from 'src/app/models/columns';
 import { Order } from './order';
 
 @Component({
@@ -11,36 +12,79 @@ export class OrdersComponent implements OnInit {
 
   constructor(private api: OrdersApiService) { }
 
-  public colOrders!:any[];
+  public colOrders: Column[] = [
+    { name: "OrderStatus", text: "Stato", visible: true},
+    { name: "OrderType", text: "Tipo",  visible: false },
+    { name: "AmazonOrderId", text: "ID Amazon", visible: false},
+    { name: "BuyerName", text: "Acquirente",  visible: true },
+    { name: "BuyerEmail", text: "Email Acquirente", visible: false},
+    { name: "EarliestShipDate", text: "Data Spedizione", visible: false},
+    { name: "FulfillmentChannel", text: "Canale di adempimento", visible: false},
+    { name: "IsBusinessOrder", text: "Ordine Business", visible: false},
+    { name: "IsPremiumOrder", text: "Ordine Premium", visible: false},
+    { name: "IsPrime", text: "Ordine Prime", visible: false},
+    { name: "IsSoldByAB", text: "Venduto da AB", visible: false},
+    { name: "MarketplaceId", text: "ID MarketPlace", visible: false},
+    { name: "NumberOfItemsShipped", text: "Ordini Spediti", visible: true},
+    { name: "NumberOfItemsUnshipped", text: "Ordini non Spediti", visible: false},
+    { name: "PaymentMethod", text: "Metodo di Pagamento",  visible: true },    
+    { name: "PaymentMethodDetails", text: "Dettagli Pagamento",  visible: true },
+    { name: "PurchaseOrderNumber", text: "Numero Acquisto",  visible: false },
+    { name: "PurchaseDate", text: "Data Acquisto",  visible: true },
+    { name: "ShipmentServiceLevelCategory", text: "Categoria Spedizione",  visible: false },
+    { name: "ShippingAddressName", text: "Indirizzo",  visible: true },
+    { name: "ShippingCityStateOrRegion", text: "Stato",  visible: true },
+    { name: "ShippingAddressCity", text: "Città",  visible: true },
+    { name: "ShippingStateOrRegionPostalCode", text: "Codice Postale",  visible: true },
+  
+  ];
+
   public listaOrders!: Order[];
   //dettagli paginazione
-  public quantita: number = 20;
-  public pagine: number = 0;
-  public ordine: string = "id"
-
+  public quantity: number = 5;
+  public page: number = 0;
+  public sort = { name: "BuyerName", orderBy: true};
+  public totalPages: number = 0;
+ 
   ngOnInit(): void {
-
-    this.colOrders = [
-      { field: "OrderStatus", header: "Stato" },
-      { field: "OrderType", header: "Tipo" },
-      { field: "BuyerName", header: "Acquirente" },
-      { field: "CompanyLegalName", header: "Ragione Sociale" },
-      { field: "PaymentMethod", header: "Metodo di Pagamento" },
-      { field: "ShippingAddressCity", header: "Città" },
-      { field: "ShippingAddressName", header: "Indirizzo" },
-      { field: "ShippingCityStateOrRegion", header: "Stato" },
-      { field: "ShippingStateOrRegionPostalCode", header: "Codice Postale" },
-    ];
-
-    this.loadOrders(this.ordine, this.pagine, this.quantita);
-
+    this.loadOrders();
   }
 
-  loadOrders(ordine: string, pagine: number, quantita: number) {
-    this.api.getAll(ordine, pagine, quantita).subscribe(resp => {
+  loadOrders() {
+    this.api.getAll(this.sort.name, this.sort.orderBy, this.page, this.quantity).subscribe((resp) => {
       this.listaOrders = resp.list;
-      console.log("listaOrders", this.listaOrders)
+      this.totalPages = resp.pagine;
     })
+   }
+
+  changePage(page: number): void{
+    this.page = page;
+    this.loadOrders();
+  }
+
+  onChangeOrderPerPage(orderPerPage: string): void{
+    this.quantity = Number(orderPerPage);
+    this.loadOrders();
+  }
+
+  isListEmpty(): boolean{
+    if(this.listaOrders?.length == 0){
+      return true;
+    } else {
+      return false;
+    }
+  } 
+  changeOrderBy(column: Column): void{
+    if (this.sort.name == column.name){
+      this.sort.orderBy = !this.sort.orderBy;
+    } else{
+      this.sort = {
+        name: column.name,
+        orderBy: true
+      };
+    }
+    
+    this.loadOrders();
   }
 
 }
