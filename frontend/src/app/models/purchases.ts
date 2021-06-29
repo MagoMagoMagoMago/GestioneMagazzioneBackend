5ef6a9b5fd2ca6ef6321f1df17f121e1d6d474f7
Dardan Baljaj <dardan.baljaj.11@gmail.com>
2021-06-29 19:10:16 +0000

    Resolve "Gestione Carello"

--- frontend/src/app/models/purchases.ts
+++ frontend/src/app/models/purchases.ts
@@ -10,3 +10,25 @@ export class Purchases {
     updateAt!: Moment;
     deletedAt!: Moment;
 }
+
+export class PurchaseInsert{
+    id!: number | null;
+    number_invoice!: number;
+    note!: string; 
+    date_invoice!: string;
+    supplier!: { id: number};
+    createdAt = null;
+    updateAt = null;
+    deletedAt = null;
+}
+
+export class PurchaseItem{ 
+    id!: number | null;
+    purchase_id!: number; 
+    item_id!: number;
+    quantity!: number;
+    unit_price!: number; 
+    created_at = null;
+    updated_at = null; 
+    deleted_at = null;
+}
