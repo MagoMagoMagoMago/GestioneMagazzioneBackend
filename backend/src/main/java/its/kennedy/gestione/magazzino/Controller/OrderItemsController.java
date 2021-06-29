1aa6128bdfa87b00f9bbfb99e2bebfd6fe7ccb5f
@michael.brotto <michael.brotto@tecnicosuperiorekennedy.it>
2021-06-29 10:14:15 +0200

    p4

--- backend/src/main/java/its/kennedy/gestione/magazzino/Controller/OrderItemsController.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/Controller/OrderItemsController.java
@@ -35,7 +35,7 @@ public class OrderItemsController {
     }
 
 	@GetMapping("between/{inizio}/{fine}")
-    public ResponseEntity<List<OrderItemDto>> getOrderItemsByAmazonOrderId(@PathVariable Instant inizio,@PathVariable Instant fine) {
+    public ResponseEntity<List<OrderItemDto>> getOrderbeetween(@PathVariable Instant inizio,@PathVariable Instant fine) {
         List<OrderItemDto> orderItemDtoList = null;
         try {
             orderItemDtoList = orderItemsService.getDateBetween(inizio, fine);
