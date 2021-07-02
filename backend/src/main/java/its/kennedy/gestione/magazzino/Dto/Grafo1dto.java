5f7ec6b68ddbca150653b2a506547e97350bdbba
@michael.brotto <michael.brotto@tecnicosuperiorekennedy.it>
2021-07-02 17:16:18 +0200

    dar

--- backend/src/main/java/its/kennedy/gestione/magazzino/Dto/Grafo1dto.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/Dto/Grafo1dto.java
@@ -5,15 +5,12 @@ import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Grafo1dto {
-	@JsonProperty("settimana")
-	private Instant settimana;
 	@JsonProperty("ricavi")
 	private Double guadagno;
 	@JsonProperty("quantita")
 	private Long quantita;
 	
-	public Grafo1dto(Integer settimana, Double guadagno,Long q) {
-		this.settimana =Instant.parse(settimana/100+"-01-01T00:00:00.00Z").plusSeconds(604800*(settimana%100));
+	public Grafo1dto(Double guadagno,Long q) {
 		this.guadagno = guadagno;
 		this.quantita=q;
 	}
--- backend/src/main/java/its/kennedy/gestione/magazzino/Dto/Grafo2dto.java
+++ backend/src/main/java/its/kennedy/gestione/magazzino/Dto/Grafo2dto.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Grafo2dto extends Grafo1dto {
 	@JsonProperty("asin")
 	private String asin;
-	public Grafo2dto(Integer settimana, Double guadagno,Long q,String asin) {
-		super(settimana, guadagno,q);
+	public Grafo2dto(Double guadagno,Long q,String asin) {
+		super(guadagno,q);
 		this.asin=asin;
 	}
 
